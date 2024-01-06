e9ead640049ec70bb692cb5142b28afcd1a8a461
ahugenb333 <ahugenberg9@gmail.com>
2024-01-06 12:59:11 -0600

    animation works great

--- app/src/main/java/com/ahugenb/tv/AnimationLoader.kt
+++ app/src/main/java/com/ahugenb/tv/AnimationLoader.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 
@@ -13,7 +14,8 @@ fun AnimationLoader(isAudioPlaying: State<Boolean>) {
     val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.void_animation))
     val progress by animateLottieCompositionAsState(
         composition = composition,
-        isPlaying = isAudioPlaying.value
+        isPlaying = isAudioPlaying.value,
+        iterations = if (isAudioPlaying.value) LottieConstants.IterateForever else 1
     )
     LottieAnimation(
         composition = composition,
