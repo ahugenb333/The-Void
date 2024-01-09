8736fcb8357c7c673e13949f9971f6d49bc52012
ahugenb333 <ahugenberg9@gmail.com>
2024-01-08 23:45:11 -0600

    list look

--- app/src/main/java/com/ahugenb/tv/shoutscreen/ShoutItemRow.kt
+++ app/src/main/java/com/ahugenb/tv/shoutscreen/ShoutItemRow.kt
@@ -1,8 +1,11 @@
 package com.ahugenb.tv.shoutscreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -10,11 +13,15 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.ahugenb.tv.R
 import com.ahugenb.tv.ShoutItem
@@ -30,10 +37,16 @@ fun ShoutItemRow(
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .height(56.dp)
             .padding(8.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = item.fileName, modifier = Modifier.weight(1f))
+        Text(
+            text = item.fileName,
+            modifier = Modifier.weight(1f),
+            style = MaterialTheme.typography.headlineSmall, // Larger text with standard typography
+            textAlign = TextAlign.Center // Center the text
+        )
 
         IconButton(onClick = {
             onPlayClicked()
@@ -56,4 +69,9 @@ fun ShoutItemRow(
             Icon(Icons.Default.Delete, contentDescription = "Delete")
         }
     }
+    Spacer(
+        modifier = Modifier.height(1.dp)
+            .fillMaxWidth()
+            .background(color = Color.White, shape = RectangleShape)
+    )
 }
