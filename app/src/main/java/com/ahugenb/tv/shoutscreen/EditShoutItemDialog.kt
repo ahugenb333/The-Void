93a078fc5fa38e09eccbe501d78452c410445b48
ahugenb333 <ahugenberg9@gmail.com>
2024-01-12 10:59:56 -0600

    rename improved

--- app/src/main/java/com/ahugenb/tv/shoutscreen/EditShoutItemDialog.kt
+++ app/src/main/java/com/ahugenb/tv/shoutscreen/EditShoutItemDialog.kt
@@ -2,11 +2,15 @@ package com.ahugenb.tv.shoutscreen
 
 import android.content.Context
 import android.os.Environment
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -40,48 +45,81 @@ fun EditShoutItemDialog(
         Column(
             modifier = Modifier
                 .padding(16.dp)
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally // Center content horizontally
         ) {
-            BasicTextField(
-                textStyle = TextStyle(color = Color.White),
-                value = editedName.value,
-                onValueChange = { it: String ->
-                    editedName.value = it
-                },
+            Surface(
                 modifier = Modifier
-                    .fillMaxWidth()
-            )
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp), // Rounded corners
+                border = BorderStroke(1.dp, Color.White) // White border
+            ) {
+                BasicTextField(
+                    value = editedName.value,
+                    onValueChange = { it: String ->
+                        editedName.value = it
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    textStyle = TextStyle(color = Color.White)
+                )
+            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(
-                onClick = {
-                    // Trim and sanitize the edited name
-                    var finalName = editedName.value.trim()
-                    if (finalName.isEmpty()) {
-                        finalName = "Untitled Shout"
-                    }
-
-                    // Check if the name exists among shout items
-                    var displayName = finalName
-                    var fileCount = 1
-                    val existingNames = existingShoutItems.map { it.displayName }
-                    while (existingNames.contains(displayName)) {
-                        fileCount++
-                        displayName = "$finalName ($fileCount)"
-                    }
-                    val fileName = displayName.plus(".mp3")
-
-                    val path = saveUpdatedItem(context, shoutItem.fileName, fileName)
-                    val updatedItem = shoutItem.copy(displayName = displayName, fileName = fileName, filePath = path)
-                    onRenameCompleted(updatedItem)
-                }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(text = "Save")
+                Button(
+                    onClick = {
+                    var finalName = editedName.value.trim()
+                        if (finalName.isEmpty()) {
+                            finalName = "Untitled Shout"
+                        }
+
+                        // Exclude the current editing item from the list of existing names
+                        val existingNames = existingShoutItems.filter { it.uuid != shoutItem.uuid }
+                            .map { it.displayName }
+                        var displayName = finalName
+                        var fileCount = 1
+
+                        while (existingNames.contains(displayName)) {
+                            fileCount++
+                            displayName = "$finalName ($fileCount)"
+                        }
+                        val fileName = displayName.plus(".mp3")
+
+                        val path = saveUpdatedItem(context, shoutItem.fileName, fileName)
+                        val updatedItem = shoutItem.copy(
+                            displayName = displayName,
+                            fileName = fileName,
+                            filePath = path
+                        )
+                        onRenameCompleted(updatedItem)
+                    },
+                    modifier = Modifier
+                        .weight(1f) // Equal weight
+                        .padding(8.dp)
+                ) {
+                    Text(text = "Save")
+                }
+
+                // Cancel Button
+                Button(
+                    onClick = { onDismiss() },
+                    modifier = Modifier
+                        .weight(1f) // Equal weight
+                        .padding(8.dp)
+                ) {
+                    Text(text = "Cancel")
+                }
             }
         }
     }
 }
+
 @Composable
 fun CustomDialog(
     onDismiss: () -> Unit,
@@ -97,11 +135,14 @@ fun CustomDialog(
         }
     }
 }
+
 fun saveUpdatedItem(context: Context, originalFileName: String, newName: String): String {
-    val recordingsDirectory = File(context.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "Recordings")
+    val recordingsDirectory =
+        File(context.getExternalFilesDir(Environment.DIRECTORY_MUSIC), "Recordings")
 
     // Path to the original file
-    val originalFile = File(recordingsDirectory, originalFileName) // assuming file extension is .mp3
+    val originalFile =
+        File(recordingsDirectory, originalFileName) // assuming file extension is .mp3
 
     // Path to the new file
     val newFile = File(recordingsDirectory, newName)
